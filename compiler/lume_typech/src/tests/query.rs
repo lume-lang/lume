cb3688dfa486cc05e55bd6d63f13efcfbbccb90f
Max T. Kristiansen <me@maxtrier.dk>
2025-05-31 17:15:10 +0200

    test(typech): update test suite to use internal HIR map

--- compiler/lume_typech/src/tests/query.rs
+++ compiler/lume_typech/src/tests/query.rs
@@ -7,7 +7,7 @@ use super::*;
 
 #[test]
 fn query_function_name_rooted() -> Result<()> {
-    let (tcx, _) = type_infer("fn foo() { }")?;
+    let tcx = type_infer("fn foo() { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
 
     assert_eq!(funcs.len(), 1);
@@ -23,7 +23,7 @@ fn query_function_name_rooted() -> Result<()> {
 
 #[test]
 fn query_function_name_rooted_name_mismatch() -> Result<()> {
-    let (tcx, _) = type_infer("fn fooo() { }")?;
+    let tcx = type_infer("fn fooo() { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
 
     assert_eq!(funcs.len(), 0);
@@ -33,7 +33,7 @@ fn query_function_name_rooted_name_mismatch() -> Result<()> {
 
 #[test]
 fn query_function_check_empty() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo() { }")?;
+    let tcx = type_infer("fn foo() { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -44,14 +44,14 @@ fn query_function_check_empty() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    assert_eq!(tcx.check_function(&hir, func, &expr)?, CallableCheckResult::Success);
+    assert_eq!(tcx.check_function(func, &expr)?, CallableCheckResult::Success);
 
     Ok(())
 }
 
 #[test]
 fn query_function_check_arg_count() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Int32) { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -63,7 +63,7 @@ fn query_function_check_arg_count() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_function(&hir, func, &expr)?,
+        tcx.check_function(func, &expr)?,
         CallableCheckResult::Failure(vec![CallableCheckError::ArgumentCountMismatch])
     );
 
@@ -72,7 +72,7 @@ fn query_function_check_arg_count() -> Result<()> {
 
 #[test]
 fn query_function_check_arg_type() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Int32) { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -84,7 +84,7 @@ fn query_function_check_arg_type() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_function(&hir, func, &expr)?,
+        tcx.check_function(func, &expr)?,
         CallableCheckResult::Failure(vec![CallableCheckError::ArgumentTypeMismatch(0)])
     );
 
@@ -93,7 +93,7 @@ fn query_function_check_arg_type() -> Result<()> {
 
 #[test]
 fn query_function_check_arg_type_second() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Boolean, y: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Boolean, y: Int32) { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -108,7 +108,7 @@ fn query_function_check_arg_type_second() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_function(&hir, func, &expr)?,
+        tcx.check_function(func, &expr)?,
         CallableCheckResult::Failure(vec![CallableCheckError::ArgumentTypeMismatch(1)])
     );
 
@@ -117,7 +117,7 @@ fn query_function_check_arg_type_second() -> Result<()> {
 
 #[test]
 fn query_function_check_arg_type_match() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Boolean, y: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Boolean, y: Int32) { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -128,14 +128,14 @@ fn query_function_check_arg_type_match() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    assert_eq!(tcx.check_function(&hir, func, &expr)?, CallableCheckResult::Success);
+    assert_eq!(tcx.check_function(func, &expr)?, CallableCheckResult::Success);
 
     Ok(())
 }
 
 #[test]
 fn query_function_check_type_arg_type_count() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo<T>() { }")?;
+    let tcx = type_infer("fn foo<T>() { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -147,7 +147,7 @@ fn query_function_check_type_arg_type_count() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_function(&hir, func, &expr)?,
+        tcx.check_function(func, &expr)?,
         CallableCheckResult::Failure(vec![CallableCheckError::TypeParameterCountMismatch])
     );
 
@@ -156,7 +156,7 @@ fn query_function_check_type_arg_type_count() -> Result<()> {
 
 #[test]
 fn query_function_check_type_arg_type_match() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo<T>() { }")?;
+    let tcx = type_infer("fn foo<T>() { }")?;
     let funcs = tcx.lookup_functions_unchecked(&SymbolName::rooted("foo"));
     let func = funcs.first().unwrap();
 
@@ -169,14 +169,14 @@ fn query_function_check_type_arg_type_match() -> Result<()> {
         }],
     };
 
-    assert_eq!(tcx.check_function(&hir, func, &expr)?, CallableCheckResult::Success);
+    assert_eq!(tcx.check_function(func, &expr)?, CallableCheckResult::Success);
 
     Ok(())
 }
 
 #[test]
 fn query_methods_on_type_empty() -> Result<()> {
-    let (tcx, _) = type_infer("struct A {} impl A {}")?;
+    let tcx = type_infer("struct A {} impl A {}")?;
 
     let ty = tcx.tcx.find_type(&SymbolName::rooted("A")).unwrap();
     let methods = tcx.methods_defined_on(&lume_types::TypeRef::new(ty.id));
@@ -188,7 +188,7 @@ fn query_methods_on_type_empty() -> Result<()> {
 
 #[test]
 fn query_methods_on_type_single() -> Result<()> {
-    let (tcx, _) = type_infer("struct A {} impl A { pub fn foo() { } }")?;
+    let tcx = type_infer("struct A {} impl A { pub fn foo() { } }")?;
 
     let ty = tcx.tcx.find_type(&SymbolName::rooted("A")).unwrap();
     let methods = tcx.methods_defined_on(&lume_types::TypeRef::new(ty.id));
@@ -206,7 +206,7 @@ fn query_methods_on_type_single() -> Result<()> {
 
 #[test]
 fn query_methods_on_type_single_impl() -> Result<()> {
-    let (tcx, _) = type_infer("struct A {} impl A { pub fn foo() { } pub fn bar() { } }")?;
+    let tcx = type_infer("struct A {} impl A { pub fn foo() { } pub fn bar() { } }")?;
 
     let ty = tcx.tcx.find_type(&SymbolName::rooted("A")).unwrap();
     let methods = tcx.methods_defined_on(&lume_types::TypeRef::new(ty.id));
@@ -226,7 +226,7 @@ fn query_methods_on_type_single_impl() -> Result<()> {
 
 #[test]
 fn query_methods_on_type_multiple_impl() -> Result<()> {
-    let (tcx, _) = type_infer("struct A {} impl A { pub fn foo() { } } impl A { pub fn bar() { } }")?;
+    let tcx = type_infer("struct A {} impl A { pub fn foo() { } } impl A { pub fn bar() { } }")?;
 
     let ty = tcx.tcx.find_type(&SymbolName::rooted("A")).unwrap();
     let methods = tcx.methods_defined_on(&lume_types::TypeRef::new(ty.id));
@@ -246,7 +246,7 @@ fn query_methods_on_type_multiple_impl() -> Result<()> {
 
 #[test]
 fn query_check_method_empty() -> Result<()> {
-    let (tcx, hir) = type_infer(
+    let tcx = type_infer(
         "struct A {}
 
         impl A {
@@ -267,7 +267,7 @@ fn query_check_method_empty() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_method(&hir, method, &lume_hir::CallExpression::Static(&expr)),
+        tcx.check_method(method, &lume_hir::CallExpression::Static(&expr)),
         Ok(CallableCheckResult::Success)
     );
 
@@ -276,7 +276,7 @@ fn query_check_method_empty() -> Result<()> {
 
 #[test]
 fn query_check_method_arg_count() -> Result<()> {
-    let (tcx, hir) = type_infer(
+    let tcx = type_infer(
         "struct A {}
 
         impl A {
@@ -297,7 +297,7 @@ fn query_check_method_arg_count() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_method(&hir, method, &lume_hir::CallExpression::Static(&expr)),
+        tcx.check_method(method, &lume_hir::CallExpression::Static(&expr)),
         Ok(CallableCheckResult::Failure(vec![
             CallableCheckError::ArgumentCountMismatch
         ]))
@@ -308,7 +308,7 @@ fn query_check_method_arg_count() -> Result<()> {
 
 #[test]
 fn query_check_method_arg_type() -> Result<()> {
-    let (tcx, hir) = type_infer(
+    let tcx = type_infer(
         "struct A {}
 
         impl A {
@@ -329,7 +329,7 @@ fn query_check_method_arg_type() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_method(&hir, method, &lume_hir::CallExpression::Static(&expr)),
+        tcx.check_method(method, &lume_hir::CallExpression::Static(&expr)),
         Ok(CallableCheckResult::Failure(vec![
             CallableCheckError::ArgumentTypeMismatch(0)
         ]))
@@ -340,7 +340,7 @@ fn query_check_method_arg_type() -> Result<()> {
 
 #[test]
 fn query_check_method_type_arg_count() -> Result<()> {
-    let (tcx, hir) = type_infer(
+    let tcx = type_infer(
         "struct A {}
 
         impl A {
@@ -361,7 +361,7 @@ fn query_check_method_type_arg_count() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_method(&hir, method, &lume_hir::CallExpression::Static(&expr)),
+        tcx.check_method(method, &lume_hir::CallExpression::Static(&expr)),
         Ok(CallableCheckResult::Failure(vec![
             CallableCheckError::TypeParameterCountMismatch
         ]))
@@ -372,7 +372,7 @@ fn query_check_method_type_arg_count() -> Result<()> {
 
 #[test]
 fn query_check_method_type_arg_valid() -> Result<()> {
-    let (tcx, hir) = type_infer(
+    let tcx = type_infer(
         "struct A {}
 
         impl A {
@@ -395,7 +395,7 @@ fn query_check_method_type_arg_valid() -> Result<()> {
     };
 
     assert_eq!(
-        tcx.check_method(&hir, method, &lume_hir::CallExpression::Static(&expr)),
+        tcx.check_method(method, &lume_hir::CallExpression::Static(&expr)),
         Ok(CallableCheckResult::Success)
     );
 
@@ -404,7 +404,7 @@ fn query_check_method_type_arg_valid() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_single() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo() { }")?;
+    let tcx = type_infer("fn foo() { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -413,7 +413,7 @@ fn query_lookup_functions_single() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_ok());
 
     let func = func.unwrap();
@@ -427,7 +427,7 @@ fn query_lookup_functions_single() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_missing() -> Result<()> {
-    let (tcx, hir) = type_infer("fn bar() { }")?;
+    let tcx = type_infer("fn bar() { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -436,7 +436,7 @@ fn query_lookup_functions_missing() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_err());
 
     let err = func.unwrap_err();
@@ -447,7 +447,7 @@ fn query_lookup_functions_missing() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_suggestion_name_mismatch() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo_() { }")?;
+    let tcx = type_infer("fn foo_() { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -456,7 +456,7 @@ fn query_lookup_functions_suggestion_name_mismatch() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_err());
 
     let err = func.unwrap_err();
@@ -467,7 +467,7 @@ fn query_lookup_functions_suggestion_name_mismatch() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_suggestion_arg_count() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Int32) { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -476,7 +476,7 @@ fn query_lookup_functions_suggestion_arg_count() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_err());
 
     let err = func.unwrap_err();
@@ -487,7 +487,7 @@ fn query_lookup_functions_suggestion_arg_count() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_suggestion_arg_mismatch() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo(x: Int32) { }")?;
+    let tcx = type_infer("fn foo(x: Int32) { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -496,7 +496,7 @@ fn query_lookup_functions_suggestion_arg_mismatch() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_err());
 
     let err = func.unwrap_err();
@@ -507,7 +507,7 @@ fn query_lookup_functions_suggestion_arg_mismatch() -> Result<()> {
 
 #[test]
 fn query_lookup_functions_suggestion_type_arg_count() -> Result<()> {
-    let (tcx, hir) = type_infer("fn foo<T>() { }")?;
+    let tcx = type_infer("fn foo<T>() { }")?;
 
     let expr = lume_hir::StaticCall {
         id: lume_span::ExpressionId::default(),
@@ -516,7 +516,7 @@ fn query_lookup_functions_suggestion_type_arg_count() -> Result<()> {
         type_arguments: Vec::new(),
     };
 
-    let func = tcx.lookup_functions(&hir, &expr);
+    let func = tcx.lookup_functions(&expr);
     assert!(func.is_err());
 
     let err = func.unwrap_err();
