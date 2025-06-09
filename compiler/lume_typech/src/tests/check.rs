9bdf2020378190f40400bc9554d31a2077fae635
Max T. Kristiansen <me@maxtrier.dk>
2025-06-09 14:17:12 +0200

    test(typech): add tests for return type checking

--- compiler/lume_typech/src/tests/check.rs
+++ compiler/lume_typech/src/tests/check.rs
@@ -17,3 +17,94 @@ fn typech_variable_decl_incompatible() {
     }"
     );
 }
+
+#[test]
+fn typech_return_value_inline_comp() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_inline_incomp() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        return true;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_condition_if() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        if false {
+            return true;
+        }
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_condition_unless() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        unless false {
+            return true;
+        }
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_condition_inf_loop() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        loop {
+            return true;
+        }
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_condition_pred_loop() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        while true {
+            return true;
+        }
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_condition_iter_loop() {
+    assert_typech_snapshot!(
+        "fn foo() -> Int32 {
+        for x in [] {
+            return true;
+        }
+        return 0_i32;
+    }"
+    );
+}
+
+#[test]
+fn typech_return_value_call_expr() {
+    assert_typech_snapshot!(
+        "fn foo() -> Boolean {
+            return false;
+        }
+
+        fn bar() -> Int32 {
+            return foo();
+        }"
+    );
+}
