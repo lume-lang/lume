9219a30ff9e7e2b201e47a36e3a609f54dbbf92d
Max T. Kristiansen <me@maxtrier.dk>
2025-07-17 13:55:56 +0200

    feat(driver): add methods to check a package without codegen

--- compiler/lume_driver/src/lib.rs
+++ compiler/lume_driver/src/lib.rs
@@ -1,11 +1,11 @@
-use std::{path::PathBuf, sync::Arc};
+use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
 use arc::locate_package;
 use error_snippet::Result;
 use lume_errors::{DiagCtx, DiagCtxHandle};
 use lume_infer::TyInferCtx;
 use lume_session::{GlobalCtx, MirPrinting, Options, Package, Session};
-use lume_span::SourceMap;
+use lume_span::{PackageId, SourceMap};
 use lume_tir::TypedIR;
 use lume_typech::TyCheckCtx;
 use lume_types::TyCtx;
@@ -40,20 +40,39 @@ impl Driver {
         Driver { package, dcx }
     }
 
-    /// Locates the [`Project`] from the given path and builds it into an executable or library.
+    /// Locates the [`Package`] from the given path and builds it into an executable or library.
     ///
     /// # Errors
     ///
     /// Returns `Err` if:
     /// - the given path has no `Arcfile` within it,
-    /// - an error occured while compiling the project,
-    /// - an error occured while linking the project
+    /// - an error occured while compiling the package,
+    /// - an error occured while linking the package
     /// - or some unexpected error occured which hasn't been handled gracefully.
     #[allow(clippy::needless_pass_by_value)]
-    pub fn build_project(root: &PathBuf, opts: Options, dcx: DiagCtxHandle) -> Result<()> {
+    pub fn build_package(root: &PathBuf, opts: Options, dcx: DiagCtxHandle) -> Result<()> {
         let driver = Self::from_root(root, dcx.clone())?;
 
         if let Err(err) = driver.build(opts) {
+            dcx.emit(err);
+        }
+
+        Ok(())
+    }
+
+    /// Locates the [`Package`] from the given path and checks it for errors.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if:
+    /// - the given path has no `Arcfile` within it,
+    /// - an error occured while compiling the package,
+    /// - or some unexpected error occured which hasn't been handled gracefully.
+    #[allow(clippy::needless_pass_by_value)]
+    pub fn check_package(root: &PathBuf, opts: Options, dcx: DiagCtxHandle) -> Result<()> {
+        let driver = Self::from_root(root, dcx.clone())?;
+
+        if let Err(err) = driver.check(opts) {
             dcx.emit(err);
         }
 
@@ -88,9 +107,40 @@ impl Driver {
 
         Ok(())
     }
+
+    /// Checks the packages within the current compiler state for errors.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if:
+    /// - an error occured while compiling the package,
+    /// - or some unexpected error occured which hasn't been handled gracefully.
+    #[tracing::instrument(skip_all, fields(root = %self.package.path.display()), err)]
+    pub fn check(mut self, options: Options) -> Result<CheckedPackageGraph> {
+        let session = Session {
+            dep_graph: std::mem::take(&mut self.package.dependencies.graph),
+            options,
+        };
+
+        let gcx = Arc::new(GlobalCtx::new(session, self.dcx.to_context()));
+        let mut dependencies = gcx.session.dep_graph.all();
+
+        // Build all the dependencies of the package in reverse, so all the
+        // dependencies without any sub-dependencies can be built first.
+        dependencies.reverse();
+
+        let mut graph = CheckedPackageGraph::default();
+
+        for dependency in dependencies {
+            let checked = Compiler::check_package(dependency, gcx.clone())?;
+
+            graph.packages.insert(checked.package, checked);
+        }
+
+        Ok(graph)
+    }
 }
 
-#[allow(unused)]
 pub struct Compiler<'a> {
     /// Defines the specific [`Package`] instance to compile.
     package: &'a Package,
@@ -128,6 +178,32 @@ impl<'a> Compiler<'a> {
         compiler.link()?;
 
         Ok(())
+    }
+
+    /// Checks the given [`Package`] for errors, such as parsing-, semantic- or configuration errors.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if:
+    /// - an error occured while compiling the project,
+    /// - or some unexpected error occured which hasn't been handled gracefully.
+    #[tracing::instrument(skip_all, fields(package = %package.name), err)]
+    pub fn check_package(package: &'a Package, gcx: Arc<GlobalCtx>) -> Result<CheckedPackage> {
+        let mut compiler = Self {
+            package,
+            gcx,
+            source_map: SourceMap::default(),
+        };
+
+        let sources = compiler.parse()?;
+        tracing::debug!(target: "driver", "finished parsing");
+
+        let (tcx, _) = compiler.type_check(sources)?;
+
+        Ok(CheckedPackage {
+            package: package.id,
+            tcx,
+        })
     }
 
     /// Returns the diagnostics context.
@@ -190,4 +266,18 @@ impl<'a> Compiler<'a> {
     fn link(&mut self) -> Result<()> {
         Ok(())
     }
+}
+
+#[derive(Default)]
+pub struct CheckedPackageGraph {
+    /// Defines map of all checked packages, keyed by their ID.
+    pub packages: HashMap<PackageId, CheckedPackage>,
+}
+
+pub struct CheckedPackage {
+    /// Defines the ID of the checked [`Package`].
+    pub package: PackageId,
+
+    /// Defines the checked type context.
+    pub tcx: TyCheckCtx,
 }
