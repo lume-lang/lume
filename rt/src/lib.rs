b737213e138178e4defdc3a186641a923852840e
Max T. Kristiansen <me@maxtrier.dk>
2025-08-09 18:38:16 +0200

    feat(rt): add runtime crate

---
+++ rt/src/lib.rs
@@ -0,0 +1,8 @@
+/// Retrieves of the type metadata of the first type parameter.
+///
+/// Since Lume passes the metadata of type arguments after all other parameters,
+/// we can safely return the first input pointer.
+#[unsafe(export_name = "std::type_of")]
+pub extern "C" fn type_of(metadata: *const ()) -> *const () {
+    metadata
+}
