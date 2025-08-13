dbd6972a09d948d8f31d4d1c7e2e8f99f5393573
Max T. Kristiansen <me@maxtrier.dk>
2025-08-13 11:33:03 +0200

    feat(std): add initial memory intrinsics

---
+++ rt/src/pointer.rs
@@ -0,0 +1,33 @@
+use crate::metadata::TypeMetadata;
+
+unsafe extern "C" {
+    pub fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
+}
+
+/// Reads an existing value from the memory location the current pointer is pointing to,
+/// offset by the given amount of bytes.
+#[unsafe(export_name = "std::mem::ptr_read")]
+#[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
+pub unsafe extern "C" fn lumert_ptr_read(ptr: *mut u8, offset: usize, metadata: *const TypeMetadata) -> *mut u8 {
+    let value_size = unsafe { metadata.read().size };
+    let dest = unsafe { crate::mem::lumert_alloc(value_size) };
+
+    unsafe { memcpy(dest, ptr.byte_add(offset), value_size) };
+
+    dest
+}
+
+/// Writes a new value to the memory location the current pointer is pointing to, offset by
+/// the given amount of bytes.
+#[unsafe(export_name = "std::mem::ptr_write")]
+#[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
+pub unsafe extern "C" fn lumert_ptr_write(
+    ptr: *mut u8,
+    value: *const u8,
+    offset: usize,
+    metadata: *const TypeMetadata,
+) {
+    let value_size = unsafe { metadata.read().size };
+
+    unsafe { memcpy(ptr.byte_add(offset), value, value_size) };
+}
