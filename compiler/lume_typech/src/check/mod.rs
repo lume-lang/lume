00fc41c961ffc2bb871730b7652d8052e9496888
Max T. Kristiansen <me@maxtrier.dk>
2025-08-23 11:46:51 +0200

    fix(typech): check signature compatibility instead of equality

--- compiler/lume_typech/src/check/mod.rs
+++ compiler/lume_typech/src/check/mod.rs
@@ -1,5 +1,5 @@
 use error_snippet::Result;
-use lume_types::TypeRef;
+use lume_types::{FunctionSig, TypeRef};
 
 use crate::TyCheckCtx;
 
@@ -160,5 +160,58 @@ impl TyCheckCtx {
         } else {
             Ok(true)
         }
+    }
+
+    /// Checks whether the given signatures are compatible. This method
+    /// expects the signatures to already be instantiated.
+    ///
+    /// **Note:** since [`FunctionSig`] don't contain the names of the functions they
+    /// represent, the equality of the names are not tested.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` when expected items cannot be found within the context.
+    #[tracing::instrument(level = "TRACE", skip(self), err, ret)]
+    pub(crate) fn check_signature_compatibility(&self, from: FunctionSig<'_>, to: FunctionSig<'_>) -> Result<bool> {
+        // If the two given signatures are exactly the same, both underlying instance and type arguments,
+        // we can be sure they're compatible.
+        if from == to {
+            return Ok(true);
+        }
+
+        for (from_param_id, to_param_id) in from.type_params.iter().zip(to.type_params.iter()) {
+            let from_param = self.tdb().type_parameter(*from_param_id).unwrap();
+            let to_param = self.tdb().type_parameter(*to_param_id).unwrap();
+
+            if from_param.constraints.len() != to_param.constraints.len() {
+                return Ok(false);
+            }
+
+            for (from_constraint, to_constraint) in from_param.constraints.iter().zip(to_param.constraints.iter()) {
+                if !self.check_type_compatibility(from_constraint, to_constraint)? {
+                    return Ok(false);
+                }
+            }
+        }
+
+        if from.params.len() != to.params.len() {
+            return Ok(false);
+        }
+
+        for (from_param, to_param) in from.params.inner().iter().zip(to.params.inner().iter()) {
+            if from_param.name == "self" && to_param.name == "self" {
+                continue;
+            }
+
+            if from_param.vararg != to_param.vararg || !self.check_type_compatibility(&from_param.ty, &to_param.ty)? {
+                return Ok(false);
+            }
+        }
+
+        if !self.check_type_compatibility(&from.ret_ty, &to.ret_ty)? {
+            return Ok(false);
+        }
+
+        Ok(true)
     }
 }
