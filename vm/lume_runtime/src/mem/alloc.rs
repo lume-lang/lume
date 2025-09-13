5d812f56582999a483903b166e31ef28e71e1ca0
Max T. Kristiansen <me@maxtrier.dk>
2025-09-13 15:18:20 +0200

    fix(std): use correct params for `alloc`, `realloc`

--- vm/lume_runtime/src/mem/alloc.rs
+++ vm/lume_runtime/src/mem/alloc.rs
@@ -1,3 +1,5 @@
+use crate::info::TypeMetadata;
+
 unsafe extern "C" {
     pub fn malloc(size: usize) -> *mut u8;
     pub fn realloc(ptr: *mut u8, new_size: usize) -> *mut u8;
@@ -8,7 +10,7 @@ unsafe extern "C" {
 /// The returned memory block may be larger than the requested size and
 /// it may not have it's content initialized and/or zeroed.
 #[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
-pub unsafe extern "C" fn lumert_alloc(size: usize) -> *mut u8 {
+pub unsafe extern "C" fn lumert_alloc(size: usize, _: *const TypeMetadata) -> *mut u8 {
     unsafe { malloc(size) }
 }
 
@@ -19,6 +21,6 @@ pub unsafe extern "C" fn lumert_alloc(size: usize) -> *mut u8 {
 /// is guaranteed to be identical to the original memory block, up until the original
 /// memory block size.
 #[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
-pub unsafe extern "C" fn lumert_realloc(ptr: *mut u8, new_size: usize) -> *mut u8 {
+pub unsafe extern "C" fn lumert_realloc(ptr: *mut u8, new_size: usize, _: *const TypeMetadata) -> *mut u8 {
     unsafe { realloc(ptr, new_size) }
 }
