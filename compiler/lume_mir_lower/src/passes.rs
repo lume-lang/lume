0770bde29363fb3428662856b4bf49b6c302f72c
Max T. Kristiansen <me@maxtrier.dk>
2025-09-15 13:46:16 +0200

    fix(mir): fix pass failing on orphan blocks

--- compiler/lume_mir_lower/src/passes.rs
+++ compiler/lume_mir_lower/src/passes.rs
@@ -6,6 +6,7 @@ use crate::FunctionTransformer;
 impl FunctionTransformer<'_, '_> {
     pub(crate) fn run_passes(&mut self) {
         DefineBlockEdges.execute(&mut self.func);
+        RemoveOrphanBlocks.execute(&mut self.func);
         DefineBlockParameters::default().execute(&mut self.func);
         PassBlockArguments::execute(&mut self.func);
         ConvertAssignmentExpressions::default().execute(&mut self.func);
@@ -53,6 +54,38 @@ impl DefineBlockEdges {
         for (succ, pred) in edges {
             func.block_mut(pred).push_predecessor(succ);
             func.block_mut(succ).push_successor(pred);
+        }
+    }
+}
+
+/// Removes all blocks, except the entry block, which have no predecessors, since
+/// they only serve to clobber future passes and codegen.
+///
+/// The pass also removes any orphan blocks as predecessor of any successor blocks.
+#[derive(Default, Debug)]
+struct RemoveOrphanBlocks;
+
+impl RemoveOrphanBlocks {
+    pub fn execute(&self, func: &mut Function) {
+        let mut orphan_blocks = Vec::new();
+
+        for block in func.blocks.values() {
+            // We ignore the entry block, since it'll never have any predecessors.
+            if block.id.0 == 0 {
+                continue;
+            }
+
+            if block.predecessors().count() == 0 {
+                orphan_blocks.push(block.id);
+            }
+        }
+
+        while let Some(orphan_block) = orphan_blocks.pop() {
+            for block in func.blocks.values_mut() {
+                block.remove_predecessor(orphan_block);
+            }
+
+            func.blocks.shift_remove(&orphan_block);
         }
     }
 }
