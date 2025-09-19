916bfe19fe435992993e3f1a21bc041867ca76d4
Max T. Kristiansen <me@maxtrier.dk>
2025-09-19 23:30:28 +0200

    refactor(arc): replace `git2` with native `git` invocation

--- compiler/arc/src/fetch/git.rs
+++ compiler/arc/src/fetch/git.rs
@@ -1,13 +1,20 @@
 use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
 use error_snippet::{IntoDiagnostic, Result, SimpleDiagnostic};
-use git2::{FetchOptions, build::RepoBuilder};
 use semver::VersionReq;
 
-use crate::{
-    deps::DependencyPath,
-    fetch::{DependencyFetcher, LOCAL_CACHE_DIR},
-};
+use crate::deps::DependencyPath;
+use crate::fetch::{DependencyFetcher, LOCAL_CACHE_DIR};
+
+/// Determines if Git is installed on the system.
+fn is_git_installed() -> bool {
+    let Ok(output) = Command::new("git").arg("--version").output() else {
+        return false;
+    };
+
+    output.status.success()
+}
 
 /// Defines a [`DependencyFetcher`] which handles dependencies which live
 /// inside of a Git repository, either local or remote.
@@ -22,6 +29,10 @@ impl DependencyFetcher for GitDependencyFetcher {
             ))
             .into());
         };
+
+        if !is_git_installed() {
+            return Err(SimpleDiagnostic::new("failed to clone repository: git is not installed").into());
+        }
 
         let repository_path = PathBuf::from(url.path());
         let repository_name = match repository_path.components().next_back() {
@@ -47,16 +58,31 @@ impl DependencyFetcher for GitDependencyFetcher {
             .into());
         }
 
-        let mut fetch_opts = FetchOptions::new();
-        fetch_opts.depth(1);
+        let mut cmd = Command::new("git");
 
-        let mut clone_builder = RepoBuilder::new();
-        clone_builder.fetch_options(fetch_opts);
+        cmd.args(["clone", "--depth", "1"]);
+        cmd.arg(url.as_str());
+        cmd.arg(&local_directory);
 
-        if let Err(err) = clone_builder.clone(url.as_str(), local_directory.as_path()) {
-            return Err(SimpleDiagnostic::new("failed to clone repository")
-                .add_cause(err.into_diagnostic())
-                .into());
+        cmd.stdin(Stdio::null());
+        cmd.stdout(Stdio::null());
+        cmd.stderr(Stdio::null());
+
+        let process = cmd.spawn().map_err(|err| {
+            Into::<error_snippet::Error>::into(SimpleDiagnostic::new(format!("failed to clone repository: {err}")))
+        })?;
+
+        let output = process
+            .wait_with_output()
+            .map_err(|err| Into::<error_snippet::Error>::into(SimpleDiagnostic::new(format!("git time-out: {err}"))))?;
+
+        if !output.status.success() {
+            return Err(SimpleDiagnostic::new(format!(
+                "git exited with status code {}\n\n{}",
+                output.status.code().unwrap_or_default(),
+                String::from_utf8_lossy(&output.stderr).trim()
+            ))
+            .into());
         }
 
         Ok(local_directory)
