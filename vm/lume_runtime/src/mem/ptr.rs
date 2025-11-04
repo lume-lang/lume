6d18860fbfc89af4094fb521b8bf4080a8e40b2f
Max T. Kristiansen <me@maxtrier.dk>
2025-11-04 17:51:40 +0100

    feat(std): box scalar values in `Array`, before writing to heap block

--- vm/lume_runtime/src/mem/ptr.rs
+++ vm/lume_runtime/src/mem/ptr.rs
@@ -1,14 +1,21 @@
 use lume_rt_metadata::TypeMetadata;
 
 unsafe extern "C" {
-    pub fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
+    pub fn memcpy(dest: *mut (), src: *const (), n: usize) -> *mut ();
+}
+
+/// Gets the given pointer as a reference, to the value which the pointer is
+/// pointing to.
+#[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
+pub unsafe extern "C" fn lumert_ptr_ref(ptr: *mut (), _metadata: *const TypeMetadata) -> *mut () {
+    ptr
 }
 
 /// Reads an existing value from the memory location the current pointer is
 /// pointing to, offset by the given amount of bytes.
 #[expect(clippy::missing_safety_doc, reason = "early alpha stage")]
 pub unsafe extern "C" fn lumert_ptr_read(ptr: *mut (), offset: usize, _metadata: *const TypeMetadata) -> *mut () {
-    unsafe { ptr.byte_add(offset) }
+    unsafe { ptr.byte_add(offset).cast::<*mut ()>().read() }
 }
 
 /// Writes a new value to the memory location the current pointer is pointing
@@ -18,11 +25,11 @@ pub unsafe extern "C" fn lumert_ptr_write(
     ptr: *mut (),
     value: *const (),
     offset: usize,
-    _metadata: *const TypeMetadata,
+    metadata: *const TypeMetadata,
 ) {
     unsafe {
-        let ptr: *mut *const () = ptr.add(offset).cast();
+        let dest: *mut *const () = ptr.byte_add(offset).cast();
 
-        *ptr = value;
+        *dest = value;
     }
 }
