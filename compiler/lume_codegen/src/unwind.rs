88af299fb0972869dac2d6ef58ac8a6ee77029ab
Max T. Kristiansen <me@maxtrier.dk>
2026-02-16 20:37:00 +0100

    chore(codegen): remove ELF-AArch64 relocation workaround

--- compiler/lume_codegen/src/unwind.rs
+++ compiler/lume_codegen/src/unwind.rs
@@ -68,12 +68,7 @@ impl RootUnwindContext {
             .object
             .add_section(segment, b".eh_frame".to_vec(), SectionKind::ReadOnlyData);
 
-        let mut eh_frame = EhFrame::from(WriterRelocate::new(
-            self.endianess,
-            product.object.architecture(),
-            product.object.format(),
-        ));
-
+        let mut eh_frame = EhFrame::from(WriterRelocate::new(self.endianess));
         self.frame_table.write_eh_frame(&mut eh_frame).unwrap();
 
         // Some unwinding implementations expect a terminating "empty" length so
