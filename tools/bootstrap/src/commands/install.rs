392a475b25da453d8815faed266bd7c5d2932d18
Max T. Kristiansen <me@maxtrier.dk>
2026-02-27 13:18:55 +0100

    feat(bootstrap): add ability to download binary toolchains

--- tools/bootstrap/src/commands/install.rs
+++ tools/bootstrap/src/commands/install.rs
@@ -1,6 +1,8 @@
+use std::path::PathBuf;
+
 use clap::CommandFactory;
 use lume_cli_tools::*;
-use lume_errors::Result;
+use lume_errors::{Result, SimpleDiagnostic};
 use owo_colors::Style;
 
 use crate::toolchain::{Profile, TargetVersion};
@@ -26,6 +28,11 @@ pub struct InstallCommand {
     /// Don't update shell configuration
     #[arg(long)]
     pub skip_shellrc: bool,
+
+    /// Allows building from source, if no binary version is available (requires
+    /// Rust toolchain)
+    #[arg(long, default_value_t)]
+    pub allow_source_build: bool,
 }
 
 impl InstallCommand {
@@ -46,49 +53,22 @@ impl InstallCommand {
             }
         };
 
-        let compiler_root = task! {
-            "cloning 'lume-lang/lume' compiler..." => {
-                crate::run_dry(|| crate::git::clone(&version))
-            },
-            Ok(path) => {
-                if crate::verbose() > 0 {
-                    format!(
-                        "cloned 'lume-lang/lume' ({})",
-                        colorized!(path.display(), Style::new().dimmed())
-                    )
-                } else {
-                    String::from("cloned 'lume-lang/lume'")
-                }
-            },
-            Err(err) => {
-                format!("failed to clone 'lume-lang/lume' ({})", err.message())
-            }
-        }?;
-
-        let opts = crate::toolchain::BuildOptions { profile: self.profile };
         let toolchain_base = crate::toolchain::artifact_directory_for(&version)?;
 
         fs::create_dir(&toolchain_base)?;
 
-        task! {
-            "building Lume compiler..." => {
-                crate::run_dry(|| crate::toolchain::compile_compiler(&compiler_root, &opts))
-            },
-            Ok(()) => "finished building Lume compiler",
-            Err(err) => {
-                format!("failed to build Lume compiler ({})", err.message())
-            }
-        }?;
-
-        task! {
-            "copying compiler artifacts..." => {
-                crate::run_dry(|| crate::toolchain::copy_artifacts(&compiler_root, &toolchain_base, &opts))
-            },
-            Ok(()) => "copied all compiler artifacts",
-            Err(err) => {
-                format!("failed to copy artifact: {}", err.message())
-            }
-        }?;
+        if let TargetVersion::Tag(tagged_version) = &version
+            && crate::fetch::is_binbuild_available(&version)?
+        {
+            fetch_binbuild(tagged_version)?;
+        } else if self.allow_source_build {
+            compile_source_tree(&version, self.profile)?;
+        } else {
+            return Err(SimpleDiagnostic::new(format!(
+                "no binary version available for {version}; source-build disabled"
+            ))
+            .into());
+        };
 
         if self.use_as_default {
             match crate::run_dry(|| crate::toolchain::link_toolchain(&toolchain_base, self.skip_shellrc)) {
@@ -103,4 +83,97 @@ impl InstallCommand {
 
         Ok(())
     }
+}
+
+/// Fetches a binary build of the given compiler version, if one is available.
+/// If not, an error is returned.
+///
+/// The returned path will be the absolute path to the extracted download
+/// directory.
+fn fetch_binbuild(version: &semver::Version) -> Result<PathBuf> {
+    let toolchain_base = crate::toolchain::artifact_directory_for(version)?;
+    fs::create_dir(&toolchain_base)?;
+
+    let download_root = task! {
+        "downloading binaries..." => {
+            crate::run_dry(|| crate::fetch::fetch(version))
+        },
+        Ok(path) => {
+            if crate::verbose() > 0 {
+                format!(
+                    "fetched lume binaries ({})",
+                    colorized!(path.display(), Style::new().dimmed())
+                )
+            } else {
+                String::from("fetched lume binaries")
+            }
+        },
+        Err(err) => {
+            format!("failed to download binaries: {}", err.message())
+        }
+    }?;
+
+    task! {
+        "copying compiler artifacts..." => {
+            crate::run_dry(|| crate::fetch::copy_artifacts(&download_root, &toolchain_base))
+        },
+        Ok(()) => "copied all compiler artifacts",
+        Err(err) => {
+            format!("failed to copy artifact: {}", err.message())
+        }
+    }?;
+
+    Ok(download_root)
+}
+
+/// Clones and compiles the source version of the given compiler version, using
+/// the provided build profile.
+///
+/// The returned path will be the absolute to the compiler root.
+fn compile_source_tree(version: &TargetVersion, profile: Profile) -> Result<PathBuf> {
+    let toolchain_base = crate::toolchain::artifact_directory_for(version)?;
+    fs::create_dir(&toolchain_base)?;
+
+    let compiler_root = task! {
+        "cloning 'lume-lang/lume' compiler..." => {
+            crate::run_dry(|| crate::git::clone(version))
+        },
+        Ok(path) => {
+            if crate::verbose() > 0 {
+                format!(
+                    "cloned 'lume-lang/lume' ({})",
+                    colorized!(path.display(), Style::new().dimmed())
+                )
+            } else {
+                String::from("cloned 'lume-lang/lume'")
+            }
+        },
+        Err(err) => {
+            format!("failed to clone 'lume-lang/lume' ({})", err.message())
+        }
+    }?;
+
+    let opts = crate::toolchain::BuildOptions { profile };
+
+    task! {
+        "building Lume compiler..." => {
+            crate::run_dry(|| crate::toolchain::compile_compiler(&compiler_root, &opts))
+        },
+        Ok(()) => "finished building Lume compiler",
+        Err(err) => {
+            format!("failed to build Lume compiler ({})", err.message())
+        }
+    }?;
+
+    task! {
+        "copying compiler artifacts..." => {
+            crate::run_dry(|| crate::toolchain::copy_artifacts(&compiler_root, &toolchain_base, &opts))
+        },
+        Ok(()) => "copied all compiler artifacts",
+        Err(err) => {
+            format!("failed to copy artifact: {}", err.message())
+        }
+    }?;
+
+    Ok(compiler_root)
 }
