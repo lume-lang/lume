85e30cb6f44a4233bd7d1465b6a45a04ace90781
Max T. Kristiansen <me@maxtrier.dk>
2026-03-11 14:52:40 +0100

    refactor(unify/engine): use lazy walking instead of subst stage

--- compiler/lume_unification/src/engine/constraints.rs
+++ compiler/lume_unification/src/engine/constraints.rs
@@ -1,165 +1,278 @@
-use indexmap::IndexSet;
-
 use crate::engine::*;
 
-pub(crate) struct LinkedVariableList<C: Context> {
-    ltr: IndexMap<TypeVar<C>, IndexSet<TypeVar<C>>>,
-    rtl: IndexMap<TypeVar<C>, IndexSet<TypeVar<C>>>,
-}
-
-impl<C: Context> LinkedVariableList<C> {
-    pub fn add_relation(&mut self, lhs: TypeVar<C>, rhs: TypeVar<C>) {
-        tracing::debug!(%lhs, %rhs, "add_resolution_link");
-
-        self.ltr.entry(lhs).or_default().insert(rhs);
-        self.rtl.entry(rhs).or_default().insert(lhs);
-    }
-
-    pub fn walk(&self, id: TypeVar<C>) -> impl Iterator<Item = TypeVar<C>> {
-        let ltr = LinkedListIter::create_from(&self.ltr, id);
-        let rtl = LinkedListIter::create_from(&self.rtl, id);
-
-        ltr.chain(rtl)
-    }
-}
-
-impl<C: Context> Default for LinkedVariableList<C> {
-    fn default() -> Self {
-        Self {
-            ltr: IndexMap::new(),
-            rtl: IndexMap::new(),
-        }
-    }
-}
-
-struct LinkedListIter<'l, C: Context> {
-    stack: smallvec::SmallVec<[TypeVar<C>; 8]>,
-    map: &'l IndexMap<TypeVar<C>, IndexSet<TypeVar<C>>>,
-}
-
-impl<'l, C: Context> LinkedListIter<'l, C> {
-    pub fn create_from(map: &'l IndexMap<TypeVar<C>, IndexSet<TypeVar<C>>>, root: TypeVar<C>) -> Self {
-        Self {
-            stack: smallvec::smallvec![root],
-            map,
-        }
-    }
-}
-
-impl<C: Context> Iterator for LinkedListIter<'_, C> {
-    type Item = TypeVar<C>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let item = self.stack.pop()?;
-        if let Some(set) = self.map.get(&item) {
-            self.stack.extend(set.iter().copied());
-        }
-
-        Some(item)
-    }
-}
-
 impl<C: Context> Engine<'_, C> {
-    #[tracing::instrument(level = "TRACE", skip_all, err(Debug))]
-    pub(crate) fn create_type_substitutions(&mut self) -> std::result::Result<(), Error<C>> {
-        let env = self.env.try_read().unwrap();
-        let linked_list = env.coalesce_type_variables(self.ctx);
+    /// Ensure there is an entry for constraints for the given type variable.
+    ///
+    /// So, even if no constraints could be generated, we would still notice
+    /// and empty constraint list and throw an error.
+    pub(crate) fn ensure_entry_for(&self, type_variable: TypeVar<C>) {
+        self.env
+            .try_write()
+            .unwrap()
+            .type_vars
+            .entry(type_variable)
+            .or_default();
+    }
 
-        let mut substitution_map = IndexMap::<TypeVar<C>, C::Ty>::new();
+    /// Create a fresh type variable for the given owner, bound to the type
+    /// variable `binding`.
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            owner = %self.ctx.name_of(owner).unwrap(),
+            binding = %self.ctx.name_of(binding).unwrap(),
+        ),
+        ret(Display)
+    )]
+    pub(crate) fn fresh_var(&mut self, owner: C::ID, binding: C::ID, location: Location) -> TypeVar<C> {
+        let tyvar = self.ctx.fresh_var(owner, binding, location);
 
-        for source_type_var in env.type_vars.keys().copied() {
-            let eq_constraints = linked_list.walk(source_type_var).flat_map(|id| {
-                env.constraints_of(id).filter_map(|constraint| {
-                    let Constraint::Equal { lhs, rhs } = constraint else {
-                        return None;
-                    };
+        self.env.try_write().unwrap().ensure_entry_for(tyvar);
 
-                    if !self.ctx.is_type_variable(rhs) {
-                        return Some((lhs, rhs));
-                    }
+        tyvar
+    }
 
-                    None
-                })
-            });
+    /// Creates a new equality containt, stating that `type_variable` must be
+    /// equal to `ty`.
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            type_var = %type_variable,
+            ty = %self.ctx.name_of_type(&ty).unwrap(),
+        )
+    )]
+    pub(crate) fn eq(&self, type_variable: TypeVar<C>, ty: C::Ty) {
+        self.env.try_write().unwrap().eq(type_variable, ty);
+    }
 
-            substitution_map.insert(
-                source_type_var,
-                normalize_equality_constraints(self.ctx, source_type_var, eq_constraints)?,
-            );
+    /// Creates a new subtyping containt, stating that the given type variable,
+    /// `type_variable`, must subtype `of`.
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            type_var = %type_variable,
+            operand = %self.ctx.name_of_type(&of).unwrap(),
+            type_parameter = %self.ctx.name_of(type_parameter).unwrap(),
+        )
+    )]
+    pub(crate) fn sub(&self, type_variable: TypeVar<C>, of: C::Ty, type_parameter: C::ID) {
+        self.env.try_write().unwrap().sub(type_variable, of, type_parameter);
+    }
+
+    /// Declares the substitution type for the given type variable.
+    ///
+    /// # Panics
+    ///
+    /// If a type has already been substituted for this type variable, this
+    /// method panics.
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            type_var = %type_variable,
+            substitute = %self.ctx.name_of_type(&with).unwrap(),
+            location = %self.ctx.span_of(type_variable.0),
+        ),
+        err(Debug)
+    )]
+    pub(crate) fn subst(&self, type_variable: TypeVar<C>, with: C::Ty) -> std::result::Result<(), Error<C>> {
+        self.occurs_check(type_variable, &with)?;
+        self.env.try_write().unwrap().subst(type_variable, with);
+
+        Ok(())
+    }
+
+    /// Checks the given resolved type for any recursion.
+    ///
+    /// Ensures that the resolved types don't contain the type variable that
+    /// they resolve. For example, adding a constraint like `T?0 = Option<T?0>`
+    /// would cause an infinite type.
+    ///
+    /// # Errors
+    ///
+    /// If the given type contains the type variable, returns
+    /// [`Error::InfiniteType`] wrapped in [`Err`].
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            type_var = %type_variable,
+            ty = %self.ctx.name_of_type(ty).unwrap(),
+        ),
+        err(Debug)
+    )]
+    fn occurs_check(&self, type_variable: TypeVar<C>, ty: &C::Ty) -> std::result::Result<(), Error<C>> {
+        if let Some(variable_env) = self.ctx.as_type_variable(ty) {
+            if variable_env.0 == type_variable.0 {
+                return Err(Error::InfiniteType {
+                    var: type_variable,
+                    ty: ty.clone(),
+                });
+            }
+
+            if let Some(resolved) = self.resolved_of(variable_env) {
+                self.occurs_check(type_variable, &resolved)?;
+            }
+
+            Ok(())
+        } else {
+            for bound_type in ty.bound_types() {
+                self.occurs_check(type_variable, bound_type)?;
+            }
+
+            Ok(())
         }
+    }
 
-        // After having unified all type variables to, hopefully, some substitute, all
-        // the other subtype constraints needs to be checked.
-        for type_var_id in env.type_vars.keys().copied() {
-            let Some(substitute) = substitution_map.get(&type_var_id) else {
-                return Err(Error::Unsolved(type_var_id));
+    /// Attempts to unify `lhs` and `rhs` to be the same type.
+    ///
+    /// If successful, the substitution chain is updated so that both types will
+    /// resolve to the same value.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error::Mismatch`] if the two types cannot be unified.
+    #[tracing::instrument(
+        level = "TRACE",
+        skip_all,
+        fields(
+            lhs = %self.ctx.name_of_type(&lhs).unwrap(),
+            rhs = %self.ctx.name_of_type(&rhs).unwrap(),
+        ),
+        err(Debug)
+    )]
+    pub(crate) fn unify(&self, lhs: C::Ty, rhs: C::Ty) -> std::result::Result<(), Error<C>> {
+        let lhs = self.walk(lhs);
+        let rhs = self.walk(rhs);
+
+        match (self.ctx.as_type_variable(&lhs), self.ctx.as_type_variable(&rhs)) {
+            // Identical type variables - no reason to bind
+            (Some(a), Some(b)) if a.0 == b.0 => Ok(()),
+
+            // Type variable on the left - regular bind
+            (Some(var), _) => self.subst(var, rhs),
+
+            // Type variable on the right - symmetric case
+            (_, Some(var)) => self.subst(var, lhs),
+
+            // Concrete types - ensure matching type argument count and unify each argument.
+            (None, None) => {
+                let lhs_bindings = lhs.bound_types().to_vec();
+                let rhs_bindings = rhs.bound_types().to_vec();
+
+                if lhs.id() != rhs.id() || lhs_bindings.len() != rhs_bindings.len() {
+                    return Err(Error::Mismatch { lhs, rhs });
+                }
+
+                for (lhs_bound_type, rhs_bound_type) in lhs_bindings.into_iter().zip(rhs_bindings.into_iter()) {
+                    self.unify(lhs_bound_type, rhs_bound_type)?;
+                }
+
+                Ok(())
+            }
+        }
+    }
+
+    /// Walks the substitution chain from the given type, until we reach either
+    /// an unresolved type variable or valid substitute.
+    pub(crate) fn walk(&self, ty: C::Ty) -> C::Ty {
+        if let Some(variable_env) = self.ctx.as_type_variable(&ty) {
+            match self.resolved_of(variable_env) {
+                Some(ty) => self.walk(ty),
+                None => ty,
+            }
+        } else {
+            ty
+        }
+    }
+
+    /// Attempts to unify all the constraints of the given type variable into a
+    /// single type.
+    fn coalesce_constraints(&self, var: TypeVar<C>) -> std::result::Result<C::Ty, Error<C>> {
+        let env = self.env.try_read().unwrap();
+        let mut result: Option<C::Ty> = None;
+
+        for constraint in env.constraints_of(var) {
+            let Constraint::Equal { ty } = constraint else {
+                continue;
             };
 
-            for constraint in env.constraints_of(type_var_id) {
-                let Constraint::Subtype { of, type_parameter_id } = constraint else {
-                    continue;
-                };
+            let resolved_type = self.walk(ty.to_owned());
 
-                if !self.ctx.implements_subtype(substitute, of) {
-                    return Err(Error::BoundUnsatisfied {
-                        ty: substitute.to_owned(),
-                        bound: of.to_owned(),
-                        type_parameter: *type_parameter_id,
-                    });
+            match result {
+                None => result = Some(resolved_type),
+                Some(existing) => {
+                    self.unify(existing, resolved_type)?;
+
+                    let resolved_var_type = self.walk(self.ctx.as_type(var));
+                    result = Some(resolved_var_type);
                 }
             }
         }
 
-        // Ensure the read-lock of `env` is dropped before
-        // adding the substitutes.
-        drop(env);
+        result.ok_or(Error::Unsolved(var))
+    }
 
-        for (type_variable, substitution) in substitution_map {
-            self.subst(type_variable, substitution);
-        }
+    pub(crate) fn solve(&self, type_var: TypeVar<C>) -> std::result::Result<(), Error<C>> {
+        let coalesced_type = self.coalesce_constraints(type_var)?;
+        let resolved_ty = self.walk(coalesced_type);
 
-        // Verify that all type variables have been resolved.
-        for (&type_variable_id, type_variable) in &self.env.try_read().unwrap().type_vars {
-            if type_variable.substitute.is_none() {
-                return Err(Error::Unsolved(type_variable_id));
+        self.subst(type_var, resolved_ty)
+    }
+
+    pub(crate) fn check_bounds_of(&self, type_var: TypeVar<C>) -> std::result::Result<(), Error<C>> {
+        let env = self.env.try_read().unwrap();
+        let type_env = env.env(type_var).unwrap();
+        let subst_type = type_env.substitute.as_ref().unwrap();
+
+        for constraint in &type_env.constraints {
+            let Constraint::Subtype { of, type_parameter } = constraint else {
+                continue;
+            };
+
+            if !self.ctx.implements_subtype(subst_type, of) {
+                return Err(Error::BoundUnsatisfied {
+                    ty: subst_type.to_owned(),
+                    bound: of.to_owned(),
+                    type_parameter: *type_parameter,
+                });
             }
         }
 
         Ok(())
     }
-}
 
-impl<C: Context> Env<C> {
-    #[tracing::instrument(level = "DEBUG", skip_all)]
-    pub fn coalesce_type_variables(&self, tcx: &C) -> LinkedVariableList<C> {
-        let mut linked_list = LinkedVariableList::default();
-
-        for (&type_var_id, type_var) in self.type_vars.iter().rev() {
-            for constraint in &type_var.constraints {
-                // INVARIANT:
-                // Type variables should always exist as the right-hand side of the constraint,
-                // since the left-hand side is meant for the "expected" type.
-                let Constraint::Equal { rhs, .. } = constraint else {
-                    continue;
-                };
-
-                let Some(constraint_variable) = tcx.as_type_variable(rhs) else {
-                    continue;
-                };
-
-                if type_var_id != constraint_variable {
-                    linked_list.add_relation(type_var_id, constraint_variable);
-                }
-            }
-        }
-
-        linked_list
+    /// Gets a vector of all type variables in the environment.
+    #[inline]
+    fn type_variables(&self) -> Vec<TypeVar<C>> {
+        self.env.try_read().unwrap().type_vars.keys().copied().collect()
     }
 
-    /// Gets an iterator of all the constraints of the given type variable.
-    fn constraints_of(&self, id: TypeVar<C>) -> impl Iterator<Item = &Constraint<C>> {
-        self.type_vars
-            .get(&id)
-            .map_or([].iter(), |type_var| type_var.constraints.iter())
+    #[tracing::instrument(level = "DEBUG", skip_all, err(Debug))]
+    fn resolve_all(&self) -> std::result::Result<(), Error<C>> {
+        for type_variable in self.type_variables() {
+            let resolved = self.resolve(type_variable)?;
+
+            self.subst(type_variable, resolved)?;
+            self.check_bounds_of(type_variable)?;
+        }
+
+        Ok(())
+    }
+
+    #[tracing::instrument(level = "DEBUG", skip_all, err(Debug))]
+    pub(crate) fn substitute_all(&self) -> std::result::Result<(), Error<C>> {
+        // Solve each variable against its constraints...
+        for type_variable in self.type_variables() {
+            self.solve(type_variable)?;
+        }
+
+        // ...then flatten all substitution chains.
+        self.resolve_all()?;
+
+        Ok(())
     }
 }
---
+++ compiler/lume_unification/src/engine/tests.rs
@@ -0,0 +1,256 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::engine::{Type as _, *};
+
+#[derive(Default, Hash, Debug, Clone, Copy, PartialEq, Eq)]
+struct ID(u64);
+
+impl ID {
+    #[inline]
+    pub fn from_name<N: Hash + ?Sized>(name: &N) -> Self {
+        Self(lume_span::hash_id(name) as u64)
+    }
+}
+
+#[derive(Default)]
+struct DummyContext {
+    next_id: u64,
+
+    /// Declares all the subtypes of each type within the context.
+    subtypes: HashMap<ID, HashSet<ID>>,
+}
+
+impl DummyContext {
+    fn next_id(&mut self) -> ID {
+        let id = ID(self.next_id + 1);
+        self.next_id += 1;
+
+        id
+    }
+
+    #[inline]
+    pub fn var(&mut self) -> TypeVar<Self> {
+        self.fresh_var(ID::default(), ID::default(), Location::empty())
+    }
+}
+
+impl Context for DummyContext {
+    type ID = ID;
+    type Ty = Type;
+
+    /// Only used for tracing
+    fn name_of(&self, _id: Self::ID) -> Result<String> {
+        Ok(String::new())
+    }
+
+    /// Only used for tracing
+    fn name_of_type(&self, _ty: &Self::Ty) -> Result<String> {
+        Ok(String::new())
+    }
+
+    /// Only used for tracing
+    fn span_of(&self, _id: Self::ID) -> Location {
+        Location::empty()
+    }
+
+    fn fresh_var(&mut self, _owner: Self::ID, _binding: Self::ID, _location: Location) -> TypeVar<Self> {
+        TypeVar(self.next_id())
+    }
+
+    fn as_type(&self, type_var: TypeVar<Self>) -> Self::Ty {
+        Type::Var(type_var.0)
+    }
+
+    fn is_type_variable(&self, ty: &Self::Ty) -> bool {
+        matches!(ty, Type::Var { .. })
+    }
+
+    fn as_type_variable(&self, ty: &Self::Ty) -> Option<TypeVar<Self>> {
+        match ty {
+            Type::Con { .. } => None,
+            Type::Var(id) => Some(TypeVar(*id)),
+        }
+    }
+
+    fn implements_subtype(&self, ty: &Self::Ty, subtype: &Self::Ty) -> bool {
+        self.subtypes
+            .get(&ty.id())
+            .is_some_and(|set| set.contains(&subtype.id()))
+    }
+}
+
+#[derive(derive_more::Debug, derive_more::PartialEq, derive_more::Eq, Hash, Clone)]
+enum Type {
+    /// Concrete type with an ID and an optional set of bound types.
+    Con {
+        id: ID,
+        kind: TypeKind,
+        bindings: Vec<Self>,
+    },
+
+    /// A potentially unsolved type variable.
+    Var(ID),
+}
+
+impl Type {
+    #[inline]
+    pub fn con0<N: Hash + ?Sized>(name: &N) -> Self {
+        Self::con(name, Vec::new())
+    }
+
+    #[inline]
+    pub fn con<N: Hash + ?Sized>(name: &N, bindings: Vec<Self>) -> Self {
+        Self::Con {
+            id: ID::from_name(name),
+            kind: TypeKind::Struct,
+            bindings,
+        }
+    }
+
+    #[inline]
+    pub fn sub0<N: Hash + ?Sized>(name: &N) -> Self {
+        Self::sub(name, Vec::new())
+    }
+
+    #[inline]
+    pub fn sub<N: Hash + ?Sized>(name: &N, bindings: Vec<Self>) -> Self {
+        Self::Con {
+            id: ID::from_name(name),
+            kind: TypeKind::Trait,
+            bindings,
+        }
+    }
+}
+
+#[derive(Hash, Debug, Clone, Copy, PartialEq, Eq)]
+enum TypeKind {
+    Struct,
+    Trait,
+}
+
+impl crate::engine::Type<DummyContext> for Type {
+    fn id(&self) -> <DummyContext as Context>::ID {
+        match self {
+            Self::Con { id, .. } | Self::Var(id) => *id,
+        }
+    }
+
+    fn bound_types(&self) -> &[Self] {
+        match self {
+            Self::Con { bindings, .. } => bindings,
+            Self::Var { .. } => &[],
+        }
+    }
+}
+
+#[test]
+fn single_equality() {
+    let mut ctx = DummyContext::default();
+
+    let t1 = Type::con0("i32");
+    let v1 = ctx.var();
+
+    let engine = Engine::new(&mut ctx);
+
+    engine.eq(v1, t1.clone());
+    engine.substitute_all().unwrap();
+
+    assert_eq!(engine.resolve(v1).unwrap(), t1);
+}
+
+#[test]
+fn variable_chaining() {
+    let mut ctx = DummyContext::default();
+
+    let v1 = ctx.var();
+    let v2 = ctx.var();
+
+    let t1 = Type::con0("i32");
+
+    let engine = Engine::new(&mut ctx);
+
+    engine.eq(v1, engine.ctx.as_type(v2));
+    engine.eq(v2, t1.clone());
+    engine.substitute_all().unwrap();
+
+    assert_eq!(engine.resolve(v1).unwrap(), t1);
+    assert_eq!(engine.resolve(v2).unwrap(), t1);
+}
+
+#[test]
+fn mismatched_instances() {
+    let mut ctx = DummyContext::default();
+
+    let var = ctx.var();
+    let con1 = Type::con0("i32");
+    let con2 = Type::con0("u32");
+
+    let engine = Engine::new(&mut ctx);
+    engine.eq(var, con1);
+    engine.eq(var, con2);
+
+    let err = engine.substitute_all().unwrap_err();
+    assert!(matches!(err, Error::Mismatch { .. }));
+}
+
+#[test]
+fn mismatched_bound_types() {
+    let mut ctx = DummyContext::default();
+
+    let var = ctx.var();
+    let con1 = Type::con("Option", vec![Type::con0("i32")]);
+    let con2 = Type::con("Option", vec![Type::con0("u32")]);
+
+    let engine = Engine::new(&mut ctx);
+    engine.eq(var, con1);
+    engine.eq(var, con2);
+
+    let err = engine.substitute_all().unwrap_err();
+    assert!(matches!(err, Error::Mismatch { .. }));
+}
+
+#[test]
+fn mismatched_bound_types_len() {
+    let mut ctx = DummyContext::default();
+
+    let var = ctx.var();
+    let con1 = Type::con("Option", vec![Type::con0("i32")]);
+    let con2 = Type::con("Option", vec![Type::con0("i32"), Type::con0("u32")]);
+
+    let engine = Engine::new(&mut ctx);
+    engine.eq(var, con1);
+    engine.eq(var, con2);
+
+    let err = engine.substitute_all().unwrap_err();
+    assert!(matches!(err, Error::Mismatch { .. }));
+}
+
+#[test]
+fn bound_unsatisfied() {
+    let mut ctx = DummyContext::default();
+
+    let var1 = ctx.var();
+    let con2 = Type::con0("Foo");
+    let obl2 = Type::sub0("Display");
+
+    let engine = Engine::new(&mut ctx);
+    engine.eq(var1, con2);
+    engine.sub(var1, obl2, ID::from_name("T"));
+
+    let err = engine.substitute_all().unwrap_err();
+    assert!(matches!(err, Error::BoundUnsatisfied { .. }));
+}
+
+#[test]
+fn infinite_type() {
+    let mut ctx = DummyContext::default();
+
+    let var1 = ctx.var();
+    let con1 = Type::con("Option", vec![ctx.as_type(var1)]);
+
+    let engine = Engine::new(&mut ctx);
+    engine.eq(var1, con1);
+
+    let err = engine.substitute_all().unwrap_err();
+    assert!(matches!(err, Error::InfiniteType { .. }));
+}
