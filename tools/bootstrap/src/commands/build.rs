9c1a02d4fd6f415453bd1bb09a245220f975175b
Max T. Kristiansen <me@maxtrier.dk>
2026-03-17 10:28:57 +0100

    refactor(cli): use `console` dotted formats for styling

---
+++ tools/bootstrap/src/commands/build.rs
@@ -0,0 +1,105 @@
+use clap::CommandFactory;
+use lume_cli_tools::*;
+use lume_errors::Result;
+
+use crate::toolchain::{Profile, TargetVersion};
+use crate::{Version, fs};
+
+#[derive(clap::Parser)]
+#[command(
+    name = "build",
+    override_usage = "lume-bootstrap build [OPTIONS] [TOOLCHAIN]",
+    about = "Builds the given toolchain from source"
+)]
+pub struct BuildCommand {
+    #[arg(long, default_value_t)]
+    pub profile: Profile,
+
+    #[command(flatten)]
+    pub version: Version,
+
+    /// Use the toolchain as the default
+    #[arg(long, default_value_t)]
+    pub use_as_default: bool,
+
+    /// Don't update shell configuration
+    #[arg(long)]
+    pub skip_shellrc: bool,
+}
+
+impl BuildCommand {
+    pub(crate) fn run(&self) -> Result<()> {
+        if self.skip_shellrc && !self.use_as_default {
+            warn!(
+                "{} has no effect without {}\n",
+                "--skip-shellrc".stylize("dim"),
+                "--use-as-default".stylize("dim")
+            );
+        }
+
+        let version = match TargetVersion::try_from(&self.version) {
+            Ok(version) => version,
+            Err(err) => {
+                let mut cmd = Self::command();
+                cmd.error(clap::error::ErrorKind::InvalidValue, err).exit();
+            }
+        };
+
+        let toolchain_base = crate::toolchain::artifact_directory_for(&version)?;
+        fs::create_dir(&toolchain_base)?;
+
+        let compiler_root = task! {
+            "cloning 'lume-lang/lume' compiler..." => {
+                crate::run_dry(|| crate::git::clone(&version))
+            },
+            Ok(path) => {
+                if crate::verbose() > 0 {
+                    format!(
+                        "cloned 'lume-lang/lume' ({})",
+                        path.display().stylize("dim")
+                    )
+                } else {
+                    String::from("cloned 'lume-lang/lume'")
+                }
+            },
+            Err(err) => {
+                format!("failed to clone 'lume-lang/lume' ({})", err.message())
+            }
+        }?;
+
+        let opts = crate::toolchain::BuildOptions { profile: self.profile };
+
+        task! {
+            "building Lume compiler..." => {
+                crate::run_dry(|| crate::toolchain::compile_compiler(&compiler_root, &opts))
+            },
+            Ok(()) => "finished building Lume compiler",
+            Err(err) => {
+                format!("failed to build Lume compiler ({})", err.message())
+            }
+        }?;
+
+        task! {
+            "copying compiler artifacts..." => {
+                crate::run_dry(|| crate::toolchain::copy_artifacts(&compiler_root, &toolchain_base, &opts))
+            },
+            Ok(()) => "copied all compiler artifacts",
+            Err(err) => {
+                format!("failed to copy artifact: {}", err.message())
+            }
+        }?;
+
+        if self.use_as_default {
+            match crate::run_dry(|| crate::toolchain::link_toolchain(&toolchain_base, self.skip_shellrc)) {
+                Ok(()) => {
+                    success!("set toolchain '{version}' as active");
+                }
+                Err(err) => {
+                    error!("failed to link toolchain: {}", err.message());
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
