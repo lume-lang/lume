b50d24dde62d410ee2644be9e0d253cb19680358
Max T. Kristiansen <me@maxtrier.dk>
2026-04-18 12:29:47 +0200

    refactor(mir/ssa): patch MIR functions instead of ugly code duplication

--- compiler/lume_mir_lower/src/pass/rename_ssa.rs
+++ compiler/lume_mir_lower/src/pass/rename_ssa.rs
@@ -1,6 +1,3 @@
-use indexmap::IndexMap;
-use lume_span::Interned;
-
 use super::*;
 
 /// Some backend implementations, specifically Cranelift, does not allow using
@@ -48,23 +45,8 @@ use super::*;
 ///     #2: i32 = *(#0, #1)
 ///     return #2
 /// ```
-///
-/// <div class="warning">
-///
-/// **Here be dragons!**
-///
-/// On a more serious note, this is quite possibly the most error-prone part of
-/// the MIR lowering process. The reason for this is mostly because of the
-/// awkward implementation of "phi" nodes between blocks in some instances.
-///
-/// This pass should receive a refactor at some point, but is currently not
-/// planned.
-///
-/// </div>
 #[derive(Default, Debug)]
-pub(crate) struct RenameSsaVariables {
-    register_counter: usize,
-}
+pub(crate) struct RenameSsaVariables;
 
 impl Pass for RenameSsaVariables {
     fn name() -> &'static str {
@@ -73,231 +55,40 @@ impl Pass for RenameSsaVariables {
 
     /// Creates a new instance of the pass without default settings.
     fn new() -> Self {
-        Self::default()
+        Self
     }
 
     /// Executes the pass on the given function.
-    fn execute(&mut self, _mcx: &MirQueryCtx, func: &mut Function) {
-        let mut new_registers = func
-            .registers
-            .iter()
-            .filter(|reg| reg.block.is_none())
-            .cloned()
-            .collect::<Vec<Register>>();
+    fn execute(&mut self, mcx: &MirQueryCtx, func: &mut Function) {
+        let mut patcher = mcx.patcher(func);
 
-        let mut register_mapping = RegisterMapping::new(func.name);
+        for parameter_idx in 0..func.signature.parameters.len() {
+            let renamed_register = patcher.next_local();
 
-        for block in func.blocks.values_mut() {
-            let block_id = block.id;
-
-            for param_idx in 0..func.signature.parameters.len() {
-                let param_reg = func.registers.register_mut(RegisterId::new(param_idx));
-
-                self.rename_register_index(param_reg.id, block_id, &mut register_mapping);
-            }
-
-            let mut block_parameters = block.parameters().collect::<Vec<RegisterId>>();
-            for param in &mut block_parameters {
-                self.rename_register_index_mut(param, block_id, &mut register_mapping);
-            }
-
-            block.parameter_replace_all(block_parameters.into_iter());
-
-            for inst in block.instructions_mut() {
-                self.update_regs_inst(inst, block_id, &mut register_mapping);
-            }
-
-            if let Some(term) = block.terminator_mut() {
-                Self::update_regs_term(term, block_id, &mut register_mapping);
-            }
-
-            for (old, new) in &register_mapping.mapping {
-                let old_reg = func.registers.register(old.0);
-
-                if new_registers.len() <= new.as_usize() {
-                    new_registers.resize_with(new.as_usize() + 1, Register::default);
-                }
-
-                new_registers[new.as_usize()] = lume_mir::Register {
-                    id: *new,
-                    ty: old_reg.ty.clone(),
-                    block: Some(block.id),
-                };
-            }
-
-            register_mapping.mapping.clear();
+            patcher.rename_register(BasicBlockId(0), RegisterId::new(parameter_idx), renamed_register);
         }
 
-        func.registers.replace_all(new_registers.into_iter());
-    }
-}
-
-impl RenameSsaVariables {
-    fn rename_register_index(
-        &mut self,
-        old: RegisterId,
-        block: BasicBlockId,
-        mapping: &mut RegisterMapping,
-    ) -> RegisterId {
-        let new = RegisterId::new(self.register_counter);
-
-        mapping.insert(block, old, new);
-        self.register_counter += 1;
-
-        new
-    }
-
-    fn rename_register_index_mut(
-        &mut self,
-        old: &mut RegisterId,
-        block: BasicBlockId,
-        mapping: &mut RegisterMapping,
-    ) -> RegisterId {
-        let new = RegisterId::new(self.register_counter);
-
-        mapping.insert(block, *old, new);
-        self.register_counter += 1;
-
-        *old = new;
-
-        new
-    }
-
-    fn update_regs_inst(&mut self, inst: &mut Instruction, block: BasicBlockId, mapping: &mut RegisterMapping) {
-        match &mut inst.kind {
-            InstructionKind::Let { register, decl, .. } => {
-                self.rename_register_index_mut(register, block, mapping);
-
-                Self::update_regs_decl(decl, block, mapping);
+        for (&block_id, block) in &func.blocks {
+            for &parameter in &block.parameters {
+                let renamed_register = patcher.next_local();
+                patcher.rename_register(block_id, parameter, renamed_register);
             }
-            InstructionKind::Allocate { register, metadata, .. } => {
-                self.rename_register_index_mut(register, block, mapping);
-                *metadata = mapping.get(block, *metadata);
-            }
-            InstructionKind::Store { target, value } | InstructionKind::StoreField { target, value, .. } => {
-                *target = mapping.get(block, *target);
-                Self::update_regs_op(value, block, mapping);
-            }
-            InstructionKind::ObjectRegister { register } => {
-                *register = mapping.get(block, *register);
-            }
-            InstructionKind::CreateSlot { .. } | InstructionKind::StoreSlot { .. } => {}
-        }
-    }
 
-    fn update_regs_term(term: &mut Terminator, block: BasicBlockId, mapping: &mut RegisterMapping) {
-        match &mut term.kind {
-            TerminatorKind::Return(op) => {
-                if let Some(value) = op {
-                    Self::update_regs_op(value, block, mapping);
-                }
-            }
-            TerminatorKind::Branch(site) => {
-                for arg in &mut site.arguments {
-                    Self::update_regs_op(arg, block, mapping);
-                }
-            }
-            TerminatorKind::ConditionalBranch {
-                condition,
-                then_block,
-                else_block,
-            } => {
-                *condition = mapping.get(block, *condition);
-
-                for arg in &mut then_block.arguments {
-                    Self::update_regs_op(arg, block, mapping);
-                }
-
-                for arg in &mut else_block.arguments {
-                    Self::update_regs_op(arg, block, mapping);
-                }
-            }
-            TerminatorKind::Switch {
-                operand,
-                arms,
-                fallback,
-            } => {
-                *operand = mapping.get(block, *operand);
-
-                for arm in arms {
-                    for arg in &mut arm.1.arguments {
-                        Self::update_regs_op(arg, block, mapping);
+            for inst in block.instructions() {
+                match &inst.kind {
+                    InstructionKind::Let { register, .. } | InstructionKind::Allocate { register, .. } => {
+                        let renamed_register = patcher.next_local();
+                        patcher.rename_register(block_id, *register, renamed_register);
                     }
-                }
-
-                for arg in &mut fallback.arguments {
-                    Self::update_regs_op(arg, block, mapping);
-                }
-            }
-            TerminatorKind::Unreachable => {}
-        }
-    }
-
-    fn update_regs_decl(decl: &mut Declaration, block: BasicBlockId, mapping: &mut RegisterMapping) {
-        match decl.kind.as_mut() {
-            DeclarationKind::Operand(operand) => {
-                Self::update_regs_op(operand, block, mapping);
-            }
-            DeclarationKind::Cast { operand, .. } => {
-                *operand = mapping.get(block, *operand);
-            }
-            DeclarationKind::Call { args, .. } | DeclarationKind::Intrinsic { args, .. } => {
-                for arg in args {
-                    Self::update_regs_op(arg, block, mapping);
-                }
-            }
-            DeclarationKind::IndirectCall { ptr, args, .. } => {
-                *ptr = mapping.get(block, *ptr);
-
-                for arg in args {
-                    Self::update_regs_op(arg, block, mapping);
+                    InstructionKind::Store { .. }
+                    | InstructionKind::StoreField { .. }
+                    | InstructionKind::ObjectRegister { .. }
+                    | InstructionKind::CreateSlot { .. }
+                    | InstructionKind::StoreSlot { .. } => {}
                 }
             }
         }
-    }
 
-    fn update_regs_op(op: &mut Operand, block: BasicBlockId, mapping: &mut RegisterMapping) {
-        match &mut op.kind {
-            OperandKind::Load { id, .. } | OperandKind::Reference { id } | OperandKind::Untagged { id } => {
-                *id = mapping.get(block, *id);
-            }
-            OperandKind::LoadField { target, .. } => {
-                *target = mapping.get(block, *target);
-            }
-            OperandKind::Bitcast { source, .. } => {
-                *source = mapping.get(block, *source);
-            }
-            OperandKind::Boolean { .. }
-            | OperandKind::Integer { .. }
-            | OperandKind::Float { .. }
-            | OperandKind::String { .. }
-            | OperandKind::LoadSlot { .. }
-            | OperandKind::SlotAddress { .. } => {}
-        }
-    }
-}
-
-struct RegisterMapping {
-    pub func_name: Interned<String>,
-    pub mapping: IndexMap<(RegisterId, BasicBlockId), RegisterId>,
-}
-
-impl RegisterMapping {
-    pub fn new(func_name: Interned<String>) -> Self {
-        Self {
-            func_name,
-            mapping: IndexMap::new(),
-        }
-    }
-
-    pub fn get(&mut self, block: BasicBlockId, id: RegisterId) -> RegisterId {
-        *self
-            .mapping
-            .get(&(id, block))
-            .unwrap_or_else(|| panic!("could not find register {id} in {block} in {}", self.func_name))
-    }
-
-    pub fn insert(&mut self, block: BasicBlockId, old: RegisterId, new: RegisterId) {
-        self.mapping.insert((old, block), new);
+        patcher.apply(func);
     }
 }
