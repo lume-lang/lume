f0086f3826f7a55de06a01754c77c2cf39cd097c
Max T. Kristiansen <me@maxtrier.dk>
2026-04-21 08:49:11 +0200

    feat(mono): monomorphize used type-references

--- compiler/lume_mono/src/collector.rs
+++ compiler/lume_mono/src/collector.rs
@@ -4,18 +4,22 @@ use lume_hir::CallExpression;
 use lume_mir_queries::MirQueryCtx;
 use lume_span::NodeId;
 use lume_typech::TyCheckCtx;
+use lume_types::TypeRef;
 
 use crate::{Generics, Instance, MonoItems};
 
+#[tracing::instrument(level = "INFO", skip_all, fields(package = mcx.tcx().current_package().name), err)]
 pub fn collect(mcx: &MirQueryCtx<'_>) -> Result<MonoItems> {
     let mut items = MonoItems::default();
     items.extend(collect_roots(mcx)?);
 
+    collect_monotypes(mcx, &mut items)?;
+
     Ok(items)
 }
 
-#[tracing::instrument(level = "INFO", skip_all, fields(package = mcx.tcx().current_package().name), err)]
-pub(crate) fn collect_roots(mcx: &MirQueryCtx<'_>) -> Result<IndexSet<Instance>> {
+#[tracing::instrument(level = "DEBUG", skip_all, fields(package = mcx.tcx().current_package().name), err)]
+fn collect_roots(mcx: &MirQueryCtx<'_>) -> Result<IndexSet<Instance>> {
     let tcx = mcx.tcx();
     let mir = mcx.mir();
 
@@ -56,17 +60,17 @@ pub(crate) fn collect_roots(mcx: &MirQueryCtx<'_>) -> Result<IndexSet<Instance>>
         tracing::debug!(item = workitem.display(tcx).to_string(), "collected");
 
         let generics = workitem.generics.clone().unwrap_or_default();
-        let type_parameters = tcx.type_params_of(workitem.id)?;
+        let type_parameters = tcx.canonical_type_parameters_of(workitem.id)?;
         debug_assert_eq!(type_parameters.len(), generics.len());
 
         for CallLocation { call_expr, callable_id } in visitor.calls_in(tcx.hir(), workitem.id)? {
-            let generic_params = tcx.type_params_of(callable_id)?;
+            let generic_params = tcx.canonical_type_parameters_of(callable_id)?;
 
             let generic_args = tcx
                 .mk_type_refs_from(call_expr.type_arguments(), call_expr.id())?
                 .into_iter()
                 .map(|type_arg| {
-                    tcx.instantiate_flat_type_from(&type_arg, type_parameters, &generics.types)
+                    tcx.instantiate_flat_type_from(&type_arg, &type_parameters, &generics.types)
                         .to_owned()
                 })
                 .collect::<Vec<_>>();
@@ -74,7 +78,7 @@ pub(crate) fn collect_roots(mcx: &MirQueryCtx<'_>) -> Result<IndexSet<Instance>>
             let call_instance = Instance {
                 id: callable_id,
                 generics: Some(Generics {
-                    ids: generic_params.to_vec(),
+                    ids: generic_params,
                     types: generic_args,
                 }),
             };
@@ -136,4 +140,111 @@ impl<'hir> lume_hir::Visitor<'hir> for CallVisitor<'_, 'hir> {
 
         Ok(())
     }
+}
+
+#[tracing::instrument(level = "DEBUG", skip_all, fields(package = mcx.tcx().current_package().name), err)]
+fn collect_monotypes(mcx: &MirQueryCtx<'_>, items: &mut MonoItems) -> Result<()> {
+    // TODO:
+    // I'd like to believe there's a better (ie. less-allocating)
+    // way of doing this.
+
+    let mut visited = IndexSet::new();
+    let mut worklist = IndexSet::new();
+
+    // NOTE:
+    // This first "seeding" of the worklist contains instances which refer to
+    // *callables*, where-as all later entries will refer to *types*.
+    for instance in items.instances.values().flatten() {
+        worklist.insert(instance.clone());
+    }
+
+    while let Some(instance) = worklist.pop() {
+        if visited.contains(&instance) {
+            continue;
+        }
+
+        let instance_generics = instance.generics.clone().unwrap_or_default();
+
+        if mcx.tcx().tdb().expect_type(instance.id).is_ok() {
+            let instance_type = TypeRef {
+                instance_of: instance.id,
+                bound_types: instance_generics.types.clone(),
+                ..Default::default()
+            };
+
+            for field in mcx.tcx().hir_fields_on(instance.id)? {
+                let field_type = mcx.tcx().mk_type_ref_from(&field.field_type, instance.id)?;
+                let field_instance = monotype_of(mcx, field_type, &instance_generics);
+
+                worklist.insert(field_instance);
+            }
+
+            for method in mcx.tcx().methods_defined_on(&instance_type) {
+                for parameter in mcx.tcx().parameters_of(method.id) {
+                    let parameter_type = mcx.tcx().mk_type_ref_from(&parameter.param_type, method.id)?;
+                    let parameter_instance = monotype_of(mcx, parameter_type, &instance_generics);
+
+                    worklist.insert(parameter_instance);
+                }
+
+                let return_type = mcx
+                    .tcx()
+                    .mk_type_ref_from(mcx.tcx().return_type_of(method.id).unwrap(), method.id)?;
+
+                let return_instance = monotype_of(mcx, return_type, &instance_generics);
+                worklist.insert(return_instance);
+            }
+
+            items.types.insert(instance_type);
+        }
+
+        for bound_type in &instance_generics.types {
+            worklist.insert(mcx.instance_of_type(bound_type.clone()));
+        }
+
+        visited.insert(instance);
+    }
+
+    for generic_type in &items.types {
+        tracing::trace!(
+            name = mcx
+                .tcx()
+                .ty_stringifier(generic_type)
+                .include_namespace(true)
+                .stringify()?,
+            "monotype"
+        );
+    }
+
+    Ok(())
+}
+
+/// Create an instantiated [`Instance`] from the given [`TypeRef`], using the
+/// generics from the surrounding context.
+fn monotype_of(mcx: &MirQueryCtx, mut type_: TypeRef, generics: &Generics) -> Instance {
+    // Replace all contained type parameters with their respective canonical type
+    // parameter. This helps with mapping the correct type arguments into the
+    // type-ref, as they might not use the same type parameter IDs.
+    for bound_type in type_.walk_mut() {
+        if !bound_type.bound_types.is_empty() || !mcx.tcx().is_type_parameter(bound_type) {
+            continue;
+        }
+
+        let Some(owner) = mcx.tcx().hir_parent_node_of(bound_type.instance_of) else {
+            continue;
+        };
+
+        if let Ok(Some(canonical)) = mcx
+            .tcx()
+            .hir_canonical_type_of(lume_hir::TypeId::from(bound_type.instance_of), owner.id())
+        {
+            bound_type.instance_of = canonical.as_node_id();
+        }
+    }
+
+    for (type_parameter_id, replacement) in generics.iter() {
+        type_.replace_contained(type_parameter_id, replacement);
+    }
+
+    mcx.instance_of_type(type_)
 }
